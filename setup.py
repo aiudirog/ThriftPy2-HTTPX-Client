0fcc8ee80be543dcc00e96f532696f956c503937
Roger Aiudi <aiudirog@gmail.com>
2021-01-26 14:06:52 -0500

    Add 3.9 and PyPy3 to testing environments

--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@ from setuptools import setup
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    install_requires=[
-        "thriftpy2>=0.4.11",
-        "httpx<0.14",
-        "typing_extensions; python_version < '3.8'",
-    ],
 )
